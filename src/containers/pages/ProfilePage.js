be00ac00c318e23b06777adb60586f7010090c32
Dalmo Mendonça <dalmo3@gmail.com>
2020-04-29 18:10:38 +1200

    some login logic

--- src/containers/pages/ProfilePage.js
+++ src/containers/pages/ProfilePage.js
@@ -1,33 +1,43 @@
-import React, { Fragment } from 'react'
-import { Typography, Button } from '@material-ui/core'
+import React, { Fragment } from 'react';
+import { Typography, Button, Tooltip } from '@material-ui/core';
 import { useAppContext } from '../AppController';
-
+import { navigate } from '@reach/router';
 
 const LogInOut = (props) => {
-  const {
-    logout,
-    loginWithPopup,
-  } = useAppContext();
+  const { logout, loginWithPopup, isVerified } = useAppContext();
 
-  return <Typography>
-    <Button
-      id={'login'}
-      variant="contained"
-      color="primary"
-      onClick={loginWithPopup}
-    >
-      Log in
-    </Button>
-    <Button id={'logout'} variant="outlined" color="primary" onClick={logout}>
-      Log out
-    </Button>
-  </Typography>
+  return (
+    <Typography>
+      <Button
+        id={'login'}
+        variant="contained"
+        color="primary"
+        onClick={loginWithPopup}
+      >
+        Log in
+      </Button>
+      <Button id={'logout'} variant="outlined" color="primary" onClick={logout}>
+        Log out
+      </Button>
+      <Button
+        id={'btn-add-new'}
+        variant="contained"
+        color="secondary"
+        disabled={!isVerified}
+        onClick={(e) => navigate('/search')}
+      >
+        Add New Business
+      </Button>
+      <Typography variant="body2">
+      {!isVerified ? 'Please verify your email first' : null}
+      </Typography>
+    </Typography>
+  );
 };
 
-
-export const ProfilePage = props => (
-<Fragment>
-  <Typography>Profile page</Typography>
-  <LogInOut/>
-</Fragment>
-)
+export const ProfilePage = (props) => (
+  <Fragment>
+    <Typography>Profile page</Typography>
+    <LogInOut />
+  </Fragment>
+);
