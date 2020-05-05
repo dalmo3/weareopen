10fb4e7457fde2f463395089b6e1b8b452cc4bdf
Dalmo Mendonça <dalmo3@gmail.com>
2020-05-06 02:20:55 +1200

    login/out flow; layouts

--- src/containers/pages/ProfilePage.js
+++ src/containers/pages/ProfilePage.js
@@ -4,26 +4,59 @@ import ResultList from '../../components/ResultList';
 import { Typography, Button, Grid } from '@material-ui/core';
 
 const LogInOut = (props) => {
-  const { isVerified, handleAddIntent } = useAppContext();
+  const {
+    isVerified,
+    isAuthenticated,
+    handleAddIntent,
+    logout,
+    loginWithPopup,
+  } = useAppContext();
 
   return (
-    <Grid container justify="flex-start" spacing={2}>
+    <Grid container direction="column" justify="flex-start" alignItems="center" spacing={1}>
       <Grid item>
         <Button
           id={'btn-add-new'}
           variant="contained"
           color="secondary"
-          disabled={!isVerified}
+          disabled={!isAuthenticated || !isVerified}
           onClick={handleAddIntent}
         >
           Add New Business
         </Button>
       </Grid>
       <Grid item>
-        <Typography variant="body2" component="span">
-          {!isVerified ? 'Please verify your email first' : null}
+        <Typography variant="body2">
+          {isAuthenticated
+            ? !isVerified
+              ? 'Please verify your email first, then log in again.'
+              : null
+            : 'Please log in to add or edit your business'}
         </Typography>
       </Grid>
+      {!isAuthenticated ? (
+        <Grid item>
+          <Button
+            id={'login'}
+            variant="contained"
+            color="secondary"
+            onClick={loginWithPopup}
+          >
+            Log in
+          </Button>
+        </Grid>
+      ) : (
+        <Grid item>
+          <Button
+            id={'logout'}
+            variant="outlined"
+            color="primary"
+            onClick={logout}
+          >
+            Log out
+          </Button>
+        </Grid>
+      )}
     </Grid>
   );
 };
@@ -31,12 +64,17 @@ const LogInOut = (props) => {
 const ProfilePage = (props) => {
   const { userMeta } = useAppContext();
   return (
-    <>
-      <Typography variant="h3">My businesses</Typography>
-      {/* <Typography>My businesses</Typography> */}
-      <ResultList results={userMeta.businesses} />
-      <LogInOut />
-    </>
+    <Grid container direction="column" spacing={1}>
+      <Grid item>
+        <Typography variant="h4">My businesses</Typography>
+      </Grid>
+      <Grid item>
+        <ResultList results={userMeta.businesses} />
+      </Grid>
+      <Grid item>
+        <LogInOut />
+      </Grid>
+    </Grid>
   );
 };
 
