99c6dba0ee30ff4f619aea37ccc8cdfc7fa4c79b
Dalmo Mendonça <dalmo3@gmail.com>
2020-05-05 17:15:44 +1200

    trying programmatic login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,6 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +26,39 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('programmatic-login', (overrides = {}) => {
+  Cypress.log({
+    name: 'loginViaAuth0',
+  });
+
+  const options = {
+    method: 'POST',
+    url: Cypress.env('auth_url'),
+    body: {
+      grant_type: 'password',
+      username: Cypress.env('auth_username'),
+      password: Cypress.env('auth_password'),
+      audience: Cypress.env('auth_audience'),
+      scope: 'openid profile email',
+      client_id: Cypress.env('auth_client_id'),
+      client_secret: Cypress.env('auth_client_secret'),
+      domain: Cypress.env('auth_domain'),
+      responseType: 'token',
+      redirect_uri: Cypress.env('auth_redirect_uri'),
+    },
+  };
+  cy.request(options);
+});
+
+
+Cypress.Commands.add('login', (overrides = {}) => {
+  Cypress.log({
+    name: 'loginViaAuth0',
+  });
+
+  // cy.wind
+
+  // window.open('','auth0:authorize:popup')
+});
